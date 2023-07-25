70b5ac95be1048e9fb60ba6e6d640fdaabb3d91d
Harsh Thaker <hst.sidhpur@gmail.com>
2023-07-25 21:46:32 +0200

    update to readme for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='yolov8face',
     version='0.0.1',
     description='A wrapper to YOLOv8 face detector.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'ultralytics',
