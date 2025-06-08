57f8b26f4819cfa365075a555abad189b2f3d0dd
Harsh Thaker <hst.sidhpur@gmail.com>
2025-06-08 09:52:05 +0200

    Add pyproject and config

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-setup(
-    name='yolov8face',
-    version='0.1.1',
-    packages=find_packages(),
-    install_requires=[
-        'ultralytics'
-    ],
-    description='Yolov8face is a Python wrapper of Ultrlytics that simplifies the process of detecting faces in images using the yolov8n-face model. It takes care of model downloading, loading and postprocessing, allowing you to detect faces in images with just a few lines of code.',
-    include_package_data=True, # Include additional files like README, LICENSE, etc.
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-)
+if __name__ == "__main__":
+    setup()
